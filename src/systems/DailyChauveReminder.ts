1ab2aa313f7b14f411111240f1248c72df92e7f7
cocosilex <cocosilex@gmail.com>
2025-09-16 21:13:01 +0200

    now send chauvereminder at fixed hour

--- src/systems/DailyChauveReminder.ts
+++ src/systems/DailyChauveReminder.ts
@@ -2,23 +2,40 @@ import { TextChannel } from 'discord.js';
 import params from '../../params.json' with { type: 'json' };
 import client from '../client.js';
 import logger from '../utils/logger.js';
+import { DateTime } from 'luxon';
 
 class DailyChauveReminder {
-    public init() {
-        setInterval(async () => {
-            await this.sendReminder();
-        }, 24 * 60 * 60 * 1000);
-
+    public async init() {
+        this.scheduleDailyReminder();
         logger.info('Daily chauve reminder initialized.');
     }
 
-    private async sendReminder() {
+    private scheduleDailyReminder() {
+        const getMsUntilNext6Paris = () => {
+            const nowParis = DateTime.now().setZone('Europe/Paris');
+            let nextUpdate = nowParis.set({ hour: 6, minute: 0, second: 0, millisecond: 0 });
+            if (nowParis >= nextUpdate) {
+                nextUpdate = nextUpdate.plus({ days: 1 });
+            }
+            return nextUpdate.toMillis() - nowParis.toMillis();
+        };
+
+        const loop = async () => {
+            await this.dailyReminder();
+            setTimeout(loop, getMsUntilNext6Paris());
+        };
+
+        setTimeout(loop, getMsUntilNext6Paris());
+    }
+
+    private async dailyReminder() {
         const channelId = params.channels.chauve;
         const channel = client.channels.cache.get(channelId) as TextChannel;
-        if (channel && channel.isTextBased()) {
-            await channel.send(`Daily chauve reminder: ${params.emojis.chauve}`);
-            await channel.send(params.emojis.chauve);
-        }
+        if (!channel || !channel.isTextBased()) return;
+
+        await channel.send(`Daily chauve reminder: ${params.emojis.chauve}`);
+        await channel.send(params.emojis.chauve);
+        logger.info('Daily chauve reminder sent.');
     }
 }
 
